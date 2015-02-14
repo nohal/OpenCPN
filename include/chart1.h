1e8bdf770792224144d1135cbd96f20bc92a3161
Pavel Kalian <pavel@kalian.cz>
2015-02-14 17:59:20 -0600

    Move the OpenCPN enums to opencpn_types.h

--- include/chart1.h
+++ include/chart1.h
@@ -98,102 +98,7 @@ class OCPN_ThreadMessageEvent;
 //    ToolBar Constants
 const int ID_TOOLBAR = 500;
 
-enum
-{
-    // The following constants represent the toolbar items (some are also used in menus).
-    // They MUST be in the SAME ORDER as on the toolbar and new items MUST NOT be added
-    // amongst them, due to the way the toolbar button visibility is saved and calculated.
-    ID_ZOOMIN = 1550,
-    ID_ZOOMOUT,
-    ID_STKUP,
-    ID_STKDN,
-    ID_ROUTE,
-    ID_FOLLOW,
-    ID_SETTINGS,
-    ID_AIS,
-    ID_ENC_TEXT,
-    ID_CURRENT,
-    ID_TIDE,
-    ID_ABOUT,
-    ID_TBEXIT,
-    ID_TBSTAT,
-    ID_PRINT,
-    ID_COLSCHEME,
-    ID_ROUTEMANAGER,
-    ID_TRACK,
-    ID_TBSTATBOX,
-    ID_MOB,
-
-    ID_PLUGIN_BASE // This MUST be the last item in the enum
-};
-
-
 static const long TOOLBAR_STYLE = wxTB_FLAT | wxTB_DOCKABLE | wxTB_TEXT ;
-
-enum
-{
-    IDM_TOOLBAR_TOGGLETOOLBARSIZE = 200,
-    IDM_TOOLBAR_TOGGLETOOLBARORIENT,
-    IDM_TOOLBAR_TOGGLETOOLBARROWS,
-    IDM_TOOLBAR_ENABLEPRINT,
-    IDM_TOOLBAR_DELETEPRINT,
-    IDM_TOOLBAR_INSERTPRINT,
-    IDM_TOOLBAR_TOGGLEHELP,
-    IDM_TOOLBAR_TOGGLE_TOOLBAR,
-    IDM_TOOLBAR_TOGGLE_ANOTHER_TOOLBAR,
-    IDM_TOOLBAR_CHANGE_TOOLTIP,
-    IDM_TOOLBAR_SHOW_TEXT,
-    IDM_TOOLBAR_SHOW_ICONS,
-    IDM_TOOLBAR_SHOW_BOTH,
-
-    ID_COMBO = 1000
-};
-
-
-// Menu item IDs for the main menu bar
-enum
-{
-    ID_MENU_ZOOM_IN = 2000,
-    ID_MENU_ZOOM_OUT,
-    ID_MENU_SCALE_IN,
-    ID_MENU_SCALE_OUT,
-
-    ID_MENU_NAV_FOLLOW,
-    ID_MENU_NAV_TRACK,
-
-    ID_MENU_CHART_NORTHUP,
-    ID_MENU_CHART_COGUP,
-    ID_MENU_CHART_QUILTING,
-    ID_MENU_CHART_OUTLINES,
-
-    ID_MENU_UI_CHARTBAR,
-    ID_MENU_UI_COLSCHEME,
-    ID_MENU_UI_FULLSCREEN,
-
-    ID_MENU_ENC_TEXT,
-    ID_MENU_ENC_LIGHTS,
-    ID_MENU_ENC_SOUNDINGS,
-    ID_MENU_ENC_ANCHOR,
-
-    ID_MENU_SHOW_TIDES,
-    ID_MENU_SHOW_CURRENTS,
-
-    ID_MENU_TOOL_MEASURE,
-    ID_MENU_ROUTE_MANAGER,
-    ID_MENU_ROUTE_NEW,
-    ID_MENU_MARK_BOAT,
-    ID_MENU_MARK_CURSOR,
-    ID_MENU_MARK_MOB,
-
-    ID_MENU_AIS_TARGETS,
-    ID_MENU_AIS_TRACKS,
-    ID_MENU_AIS_CPADIALOG,
-    ID_MENU_AIS_CPASOUND,
-    ID_MENU_AIS_TARGETLIST,
-    ID_MENU_OQUIT
-};
-
-
 
 #define N_STATUS_BAR_FIELDS_MAX     20
 
@@ -537,27 +442,6 @@ class MyPrintout: public wxPrintout
   
 private:
   wxBitmap m_GLbmp;
-
-};
-
-
-//      A global definition for window, timer and other ID's as needed.
-enum {
-    ID_NMEA_WINDOW      = wxID_HIGHEST,
-    ID_AIS_WINDOW,
-    INIT_TIMER,
-    FRAME_TIMER_1,
-    FRAME_TIMER_2,
-    TIMER_AIS1,
-    TIMER_DSC,
-    TIMER_AISAUDIO,
-    AIS_SOCKET_ID,
-    FRAME_TIMER_DOG,
-    FRAME_TC_TIMER,
-    FRAME_COG_TIMER,
-    MEMORY_FOOTPRINT_TIMER,
-    BELLS_TIMER,
-    ID_NMEA_THREADMSG
 
 };
 
--- include/ocpn_types.h
+++ include/ocpn_types.h
@@ -20,7 +20,7 @@
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, write to the                         *
  *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.             *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
  ***************************************************************************
  *
  *
@@ -39,7 +39,8 @@
 
 WX_DEFINE_ARRAY_INT(int, ArrayOfInts);
 
-//    ChartType constants
+/** \brief ChartType constants
+ */
 typedef enum ChartTypeEnum
 {
       CHART_TYPE_UNKNOWN = 0,
@@ -53,7 +54,8 @@ typedef enum ChartTypeEnum
       CHART_TYPE_PLUGIN
 }_ChartTypeEnum;
 
-//    ChartFamily constants
+/** \brief ChartFamily constants
+ */
 typedef enum ChartFamilyEnum
 {
       CHART_FAMILY_UNKNOWN = 0,
@@ -62,6 +64,8 @@ typedef enum ChartFamilyEnum
       CHART_FAMILY_DONTCARE
 }_ChartFamilyEnum;
 
+/** \brief ColorScheme constants
+ */
 typedef enum ColorScheme
 {
       GLOBAL_COLOR_SCHEME_RGB,
@@ -93,5 +97,105 @@ typedef struct {
     time_t FixTime;
     int    nSats;
 } GenericPosDatEx;
+
+
+/** \brief IDs for the toolbar items
+ * Some of them are also used in menus
+ */
+typedef enum ToolIdEnum
+{
+    // The following constants represent the toolbar items (some are also used in menus).
+    // They MUST be in the SAME ORDER as on the toolbar and new items MUST NOT be added
+    // amongst them, due to the way the toolbar button visibility is saved and calculated.
+    ID_ZOOMIN = 1550,
+    ID_ZOOMOUT,
+    ID_STKUP,
+    ID_STKDN,
+    ID_ROUTE,
+    ID_FOLLOW,
+    ID_SETTINGS,
+    ID_AIS,
+    ID_ENC_TEXT,
+    ID_CURRENT,
+    ID_TIDE,
+    ID_ABOUT,
+    ID_TBEXIT,
+    ID_TBSTAT,
+    ID_PRINT,
+    ID_COLSCHEME,
+    ID_ROUTEMANAGER,
+    ID_TRACK,
+    ID_TBSTATBOX,
+    ID_MOB,
+
+    ID_PLUGIN_BASE // This MUST be the last item in the enum
+}_ToolIdEnum;
+
+
+/** \brief Menu item IDs for the main menu bar
+ */
+typedef enum MenuIdEnum
+{
+    ID_MENU_ZOOM_IN = 2000,
+    ID_MENU_ZOOM_OUT,
+    ID_MENU_SCALE_IN,
+    ID_MENU_SCALE_OUT,
+
+    ID_MENU_NAV_FOLLOW,
+    ID_MENU_NAV_TRACK,
+
+    ID_MENU_CHART_NORTHUP,
+    ID_MENU_CHART_COGUP,
+    ID_MENU_CHART_QUILTING,
+    ID_MENU_CHART_OUTLINES,
+
+    ID_MENU_UI_CHARTBAR,
+    ID_MENU_UI_COLSCHEME,
+    ID_MENU_UI_FULLSCREEN,
+
+    ID_MENU_ENC_TEXT,
+    ID_MENU_ENC_LIGHTS,
+    ID_MENU_ENC_SOUNDINGS,
+    ID_MENU_ENC_ANCHOR,
+
+    ID_MENU_SHOW_TIDES,
+    ID_MENU_SHOW_CURRENTS,
+
+    ID_MENU_TOOL_MEASURE,
+    ID_MENU_ROUTE_MANAGER,
+    ID_MENU_ROUTE_NEW,
+    ID_MENU_MARK_BOAT,
+    ID_MENU_MARK_CURSOR,
+    ID_MENU_MARK_MOB,
+
+    ID_MENU_AIS_TARGETS,
+    ID_MENU_AIS_TRACKS,
+    ID_MENU_AIS_CPADIALOG,
+    ID_MENU_AIS_CPASOUND,
+    ID_MENU_AIS_TARGETLIST,
+    ID_MENU_OQUIT
+}_MenuIdEnum;
+
+
+/** \brief A global definition for window, timer and other ID's as needed.
+ */
+typedef enum GlobalIdEnum
+{
+    ID_NMEA_WINDOW = wxID_HIGHEST,
+    ID_AIS_WINDOW,
+    INIT_TIMER,
+    FRAME_TIMER_1,
+    FRAME_TIMER_2,
+    TIMER_AIS1,
+    TIMER_DSC,
+    TIMER_AISAUDIO,
+    AIS_SOCKET_ID,
+    FRAME_TIMER_DOG,
+    FRAME_TC_TIMER,
+    FRAME_COG_TIMER,
+    MEMORY_FOOTPRINT_TIMER,
+    BELLS_TIMER,
+    ID_NMEA_THREADMSG
+}_GlobalIdEnum;
 
 #endif
