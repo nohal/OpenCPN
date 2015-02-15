b48162827a3a9c146e332ffe76aae6e43b2341b1
Pavel Kalian <pavel@kalian.cz>
2015-02-14 18:00:11 -0600

    Eliminate the unused DummyTextCtrl class

--- include/chart1.h
+++ include/chart1.h
@@ -445,23 +445,6 @@ private:
 
 };
 
-//-----------------------------------------------------------------------
-//          Dummy Text Control for global key events
-//-----------------------------------------------------------------------
-class DummyTextCtrl: public wxTextCtrl
-{
-public:
-      DummyTextCtrl(wxWindow *parent, wxWindowID id);
-      void OnChar(wxKeyEvent &event);
-      void OnMouseEvent(wxMouseEvent& event);
-
-      wxTimer     m_MouseWheelTimer;
-      int         m_mouse_wheel_oneshot;
-      int         m_last_wheel_dir;
-
-      DECLARE_EVENT_TABLE()
-};
-
 
 extern int OCPNMessageBox(wxWindow *parent,
                           const wxString& message,
