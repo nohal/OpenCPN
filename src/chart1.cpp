c3cec94d7c1edc9f5b48cb7a4f22c5a86426dbc9
Pavel Kalian <pavel@kalian.cz>
2016-04-21 17:14:19 -0500

    Fix the build using GCC

--- src/chart1.cpp
+++ src/chart1.cpp
@@ -6121,7 +6121,7 @@ void MyFrame::OnInitTimer(wxTimerEvent& event)
                 
                 if( cp->bEnabled ) {
 #ifdef __OCPN_USE_WEBSOCKETS__
-                    if( cp->NetProtocol == NetworkProtocol::SIGNALK )
+                    if( cp->NetProtocol == SIGNALK )
                     {
                         if( cp->bEnabled )
                             g_pMUX->AddWSStream(cp);
