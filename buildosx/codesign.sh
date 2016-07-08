94ec0408aa2b1c6b34d3d770c4cb68acd8efedd2
Pavel Kalian <pavel@kalian.cz>
2016-07-08 17:07:28 -0300

    Fix wxWidgets before creating MacOS bundle and optionally sign it

--- buildosx/codesign.sh
+++ buildosx/codesign.sh
@@ -6,11 +6,11 @@ if [ $# -eq 3 ]; then
     echo "Going to codesign the DMG image."
   else
     echo "Not signing anything, provide a valid identity by setting -DCODESIGN_IDENTITY=\"Some identity from your keychain\" while running cmake."
-    exit 1
+    exit 0
   fi
 else
   echo "Not going to sign anything, provide a valid identity by setting -DCODESIGN_IDENTITY=\"Some identity from your keychain\" while running cmake."
-  exit 1
+  exit 0
 fi
 if [ "$1" == "BUNDLE" ]; then
   codesign --deep --force -s "$2" -i org.opencpn.opencpn $3
