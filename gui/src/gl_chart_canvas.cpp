3ec6ba2fe3cbca623a2cfaef2e6639eda39b5a0e
Pavel Kalian <pavel@kalian.cz>
2026-07-01 20:30:23 -0300

    Fix the edge of the screen repeated artifacts while panning

--- gui/src/gl_chart_canvas.cpp
+++ gui/src/gl_chart_canvas.cpp
@@ -678,6 +678,10 @@ bool glChartCanvas::buildFBOSize(int fboSize) {
                     GL_NEAREST);
     glTexParameteri(g_texture_rectangle_format, GL_TEXTURE_MAG_FILTER,
                     GL_NEAREST);
+    glTexParameteri(g_texture_rectangle_format, GL_TEXTURE_WRAP_S,
+                    GL_CLAMP_TO_EDGE);
+    glTexParameteri(g_texture_rectangle_format, GL_TEXTURE_WRAP_T,
+                    GL_CLAMP_TO_EDGE);
     glTexImage2D(g_texture_rectangle_format, 0, GL_RGBA, m_cache_tex_x,
                  m_cache_tex_y, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
   }
@@ -849,6 +853,10 @@ bool glChartCanvas::buildFBOSize(int fboSize) {
                     GL_NEAREST);
     glTexParameteri(g_texture_rectangle_format, GL_TEXTURE_MAG_FILTER,
                     GL_NEAREST);
+    glTexParameteri(g_texture_rectangle_format, GL_TEXTURE_WRAP_S,
+                    GL_CLAMP_TO_EDGE);
+    glTexParameteri(g_texture_rectangle_format, GL_TEXTURE_WRAP_T,
+                    GL_CLAMP_TO_EDGE);
     glTexImage2D(g_texture_rectangle_format, 0, GL_RGBA, m_cache_tex_x,
                  m_cache_tex_y, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
   }
@@ -980,6 +988,8 @@ void glChartCanvas::BuildFBO() {
   glBindTexture(GL_TEXTURE_2D, m_basemap_tex);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_cache_tex_x, m_cache_tex_y, 0,
                GL_RGBA, GL_UNSIGNED_BYTE, NULL);
   glGenFramebuffers(1, &m_basemap_fbo);
